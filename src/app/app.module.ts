6e5f68a883e79af40005e58f3bdd5c17bad62a40
Biraj <biraj01@gmail.com>
2020-09-22 11:32:59 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
